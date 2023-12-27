97363a82fe8975a4e6db1d5dd9333cbd18051213
Atharva Ingale <akingale00@gmail.com>
2023-12-27 22:58:03 +0530

    Updates

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-project_name= "Chicken Disease Classifier"
+project_name= "ChickenDiseaseClassifier"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
