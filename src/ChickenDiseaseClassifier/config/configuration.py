76134a7603cf60eb96d66147da145efa23a21164
Atharva Ingale <akingale00@gmail.com>
2024-01-14 22:15:28 +0530

    updates

--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,5 +1,6 @@
 from ChickenDiseaseClassifier.constants import *
 from ChickenDiseaseClassifier.utils.common import read_yaml, create_directories
+from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig
 
 class ConfigurationManager:
     def __init__(
