9dfad338f39aedcc3d8beaedcd219a02ec942be7
Atharva Ingale <akingale00@gmail.com>
2024-01-15 20:45:23 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        data_ingestion = DataIngestionTrainingPipeline()
-        data_ingestion.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from ChickenDiseaseClassifier.constants import *
 from ChickenDiseaseClassifier.utils.common import read_yaml, create_directories
 from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig
+from ChickenDiseaseClassifier.entity.config_entity import PrepareBaseModelConfig
 
 class ConfigurationManager:
     def __init__(
@@ -28,3 +29,32 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+class ConfigurationManager:
+    def __init__(
+        self, 
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        create_directories([self.config.artifacts_root])
+
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
